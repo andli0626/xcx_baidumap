7f32ac634410c022db6ada23f3d22217b5bfc946
李林 <ww1095@163.com>
2017-07-27 17:02:39 +0800

    查询 美食

---
+++ pages/search/search.js
@@ -0,0 +1,76 @@
+var bmap = require('../../libs/bmap-wx.min.js');
+var wxMarkerData = [];
+Page({
+  data: {
+    markers: [],
+    latitude: '',
+    longitude: '',
+    placeData: {}
+  },
+  // 点击地图
+  makertap: function (e) {
+    var that = this;
+    var id = e.markerId;
+    that.showSearchInfo(wxMarkerData, id);      // 显示 兴趣点信息
+    that.changeMarkerColor(wxMarkerData, id);   // 改变 选中兴趣点颜色
+  },
+  // 页面加载
+  onLoad: function () {
+    var that = this;
+    var BMap = new bmap.BMapWX({
+      ak: 'Sen3NdHvcNdy1A4DQQzHbXfU9hrrGp6P' // 创建应用获得ak
+    });
+    var fail = function (data) {
+      console.log(data)
+    };
+    var success = function (data) {
+      wxMarkerData = data.wxMarkerData;
+      that.setData({
+        markers: wxMarkerData
+      });
+      that.setData({
+        latitude: wxMarkerData[0].latitude
+      });
+      that.setData({
+        longitude: wxMarkerData[0].longitude
+      });
+    }
+    // 搜索美食
+    BMap.search({
+      "query"     : '美食',
+      fail        : fail,        // 失败回调
+      success     : success,     // 成功回调
+      iconPath    : '../../img/marker_red.png', // 默认图标
+      iconTapPath : '../../img/marker_red.png'  // 选中图标
+    });
+  },
+
+  // 显示 选中点信息
+  showSearchInfo: function (data, i) {
+    var that = this;
+    that.setData({
+      placeData: {
+        title     : '名称：' + data[i].title + '\n',
+        address   : '地址：' + data[i].address + '\n',
+        telephone : '电话：' + data[i].telephone
+      }
+    });
+  },
+
+  // 改变 兴趣点 颜色
+  changeMarkerColor: function (data, id) {
+    var that = this;
+    var markersTemp = [];
+    for (var i = 0; i < data.length; i++) {
+      if (i === id) {
+        data[i].iconPath = "../../img/marker_yellow.png";
+      } else {
+        data[i].iconPath = "../../img/marker_red.png";
+      }
+      markersTemp[i] = data[i];
+    }
+    that.setData({
+      markers: markersTemp
+    });
+  }
+})
